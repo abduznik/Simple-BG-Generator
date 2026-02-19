d57a26e8e2bf6ed4af8057d82f1de4c3995591c5
abduznik <85239936+abduznik@users.noreply.github.com>
2026-02-19 19:42:58 +0200

    update configuration for Simple-BG-Generator

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/background_generator/',
+  base: '/Simple-BG-Generator/',
   plugins: [
     react(),
     tailwindcss(),
